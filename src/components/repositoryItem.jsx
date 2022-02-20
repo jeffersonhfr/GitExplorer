68b36d2f9266d94646ba5fd44c4a4a8a4bc8a98c
Jefferson Rodrigues <jeffersonhfr@gmail.com>
2022-02-20 18:31:12 -0300

    estrutura do projeto pronta já com TS

--- src/components/repositoryItem.jsx
+++
@@ -1,9 +0,0 @@
-export function RepositoryItem(props) {
-  return (
-    <li>
-      <strong>{props.resposity?.name ?? ''}</strong>
-      <p>{props.resposity?.description ?? 'Default'}</p>
-      <a href={props.resposity?.link ?? 'Default'}>Acessar Repositório</a>
-    </li>
-  );
-}
